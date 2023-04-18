d97127621c3121c4cb2921a42f3f3a63d53b7d1e
Bruno Rocha Mantovani <bruno.trabalho449243@outlook.com>
2023-04-18 11:58:07 -0300

    Projeto Iniciado

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,25 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-
+import { NavbarComponent } from './navbar/navbar.component';
+import { LoginPageComponent } from './pages/accounts/login-page/login-page.component';
+import { ResetPasswordPageComponent } from './pages/accounts/reset-password-page/reset-password-page.component';
+import { SignupPageComponent } from './pages/accounts/signup-page/signup-page.component';
+import { PetsPageComponent } from './pages/accounts/pets-page/pets-page.component';
+import { ProductsPageComponent } from './pages/store/products-page/products-page.component';
+import { CartPageComponent } from './pages/store/cart-page/cart-page.component';
+import { FramePageComponent } from './pages/master/frame.page'
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    LoginPageComponent,
+    ResetPasswordPageComponent,
+    SignupPageComponent,
+    PetsPageComponent,
+    ProductsPageComponent,
+    CartPageComponent,
+    FramePageComponent
   ],
   imports: [
     BrowserModule,
