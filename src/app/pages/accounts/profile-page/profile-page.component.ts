aeb778cb2845295dc1613f7e71fd2041b3d1a114
Bruno Rocha Mantovani <bruno.trabalho449243@outlook.com>
2023-05-08 16:48:27 -0300

    Atualizado

--- src/app/pages/accounts/profile-page/profile-page.component.ts
+++ src/app/pages/accounts/profile-page/profile-page.component.ts
@@ -1,15 +1,51 @@
-import { Component } from '@angular/core';
-import { Form, FormBuilder, FormGroup } from '@angular/forms';
+import { Component, OnInit } from '@angular/core';
+import { Form, FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
+import { ToastrService } from 'ngx-toastr';
+import { DataService } from 'src/app/services/data.service';
 
 @Component({
   selector: 'app-profile-page',
   templateUrl: './profile-page.component.html',
   styleUrls: ['./profile-page.component.css']
 })
-export class ProfilePageComponent {
-//  public form : FormGroup;
-//  public busy = false;
-//  constructor(private fb : FormBuilder){
-//   this
-//  }
+export class ProfilePageComponent implements OnInit {
+  public form: FormGroup;
+  public busy = false;
+  constructor(private fb: FormBuilder,
+    private router: Router,
+    private toastr: ToastrService,
+    private service: DataService) {
+    this.form = this.fb.group({
+      nam: ['', Validators.compose([
+        Validators.minLength(3),
+        Validators.maxLength(80),
+        Validators.required
+      ])],
+      document: [{ value: '', disabled: true }],
+      email: ['', Validators.compose([
+        Validators.minLength(5),
+        Validators.maxLength(120),
+        Validators.required
+      ])],
+    })
+  }
+  ngOnInit() {
+    this.busy = true;
+    this
+      .service
+      .getProfile()
+      .subscribe(
+        (data: any) => {
+          this.busy = false;
+          this.form.controls['name'].setValue(data.name);
+          this.form.controls['document'].setValue(data.document);
+          this.form.controls['email'].setValue(data.email);
+        },
+        (err) => {
+          console.log(err);
+          this.busy = false;
+        }
+      )
+  }
 }
